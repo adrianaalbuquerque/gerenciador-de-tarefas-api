cefee20adde6130f3f381fcd14e05ae8d50ed746
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-05-18 20:17:45 -0300

    Commit inicial

---
+++ src/TaskManagerAPI/TaskManager.Domain/Entidades/Tarefa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Domain.Entities
+{
+    public class Tarefa
+    {
+        public Tarefa(int id, string titulo, string descricao, string status, string data, string dataConclusao) { }
+
+        public void IniciarTarefa() { }
+        public void ConcluirTarefa() { }
+        public void EditarTarefa() { }
+        public void ApagarTarefa() { }
+
+    }
+}
---
+++ src/TaskManagerAPI/TaskManager.Domain/Enum/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoDeTarefa.Dominio.Enum
+{
+    public enum StatusTarefa
+    {
+        Pendente,
+        EmAndamento,
+        Concluida
+    }
+}
